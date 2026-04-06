98baef911f73d308e9c5755902cebdc2df44fc45
sxkkrn <dareaf078@gmail.com>
2026-04-06 10:00:27 +0900

    単語アプリ更新

---
+++ words_en_others.js
@@ -0,0 +1,76 @@
+const words_en_others = [
+  { en:"according to", jp:["〜によると"], pos:"その他", category:"会話" },
+  { en:"after all", jp:["結局"], pos:"その他", category:"会話" },
+  { en:"all of a sudden", jp:["突然"], pos:"その他", category:"時間" },
+  { en:"as a result", jp:["その結果"], pos:"その他", category:"会話" },
+  { en:"as well as", jp:["〜だけでなく"], pos:"その他", category:"会話" },
+  { en:"at least", jp:["少なくとも"], pos:"その他", category:"程度" },
+  { en:"at most", jp:["多くても"], pos:"その他", category:"程度" },
+
+  { en:"because of", jp:["〜のために"], pos:"その他", category:"会話" },
+  { en:"by means of", jp:["〜によって"], pos:"その他", category:"会話" },
+  { en:"by no means", jp:["決して〜ない"], pos:"その他", category:"強調" },
+  { en:"by the time", jp:["〜する頃までに"], pos:"その他", category:"時間" },
+
+  { en:"compared to", jp:["〜と比べて"], pos:"その他", category:"会話" },
+  { en:"contrary to", jp:["〜に反して"], pos:"その他", category:"会話" },
+
+  { en:"depending on", jp:["〜次第で"], pos:"その他", category:"条件" },
+  { en:"despite", jp:["〜にもかかわらず"], pos:"その他", category:"接続" },
+  { en:"due to", jp:["〜が原因で"], pos:"その他", category:"会話" },
+
+  { en:"even though", jp:["〜にもかかわらず"], pos:"その他", category:"接続" },
+  { en:"ever since", jp:["それ以来ずっと"], pos:"その他", category:"時間" },
+
+  { en:"for instance", jp:["例えば"], pos:"その他", category:"会話" },
+  { en:"for the sake of", jp:["〜のために"], pos:"その他", category:"会話" },
+
+  { en:"in addition", jp:["さらに"], pos:"その他", category:"会話" },
+  { en:"in advance", jp:["前もって"], pos:"その他", category:"時間" },
+  { en:"in case", jp:["〜に備えて"], pos:"その他", category:"条件" },
+  { en:"in contrast", jp:["対照的に"], pos:"その他", category:"会話" },
+  { en:"in detail", jp:["詳しく"], pos:"その他", category:"会話" },
+  { en:"in fact", jp:["実際には"], pos:"その他", category:"会話" },
+  { en:"in general", jp:["一般的に"], pos:"その他", category:"会話" },
+  { en:"in order to", jp:["〜するために"], pos:"その他", category:"目的" },
+  { en:"in other words", jp:["言い換えれば"], pos:"その他", category:"会話" },
+  { en:"in particular", jp:["特に"], pos:"その他", category:"強調" },
+  { en:"in spite of", jp:["〜にもかかわらず"], pos:"その他", category:"接続" },
+
+  { en:"instead of", jp:["〜の代わりに"], pos:"その他", category:"会話" },
+
+  { en:"no matter", jp:["たとえ〜でも"], pos:"その他", category:"接続" },
+  { en:"not only A but also B", jp:["AだけでなくBも"], pos:"その他", category:"構文" },
+
+  { en:"on the contrary", jp:["それどころか"], pos:"その他", category:"会話" },
+  { en:"on the other hand", jp:["一方で"], pos:"その他", category:"会話" },
+  { en:"on behalf of", jp:["〜を代表して"], pos:"その他", category:"会話" },
+
+  { en:"rather than", jp:["〜よりむしろ"], pos:"その他", category:"比較" },
+  { en:"regardless of", jp:["〜に関係なく"], pos:"その他", category:"条件" },
+
+  { en:"so that", jp:["〜するように"], pos:"その他", category:"目的" },
+  { en:"such as", jp:["例えば〜など"], pos:"その他", category:"会話" },
+
+  { en:"thanks to", jp:["〜のおかげで"], pos:"その他", category:"会話" },
+  { en:"that is to say", jp:["つまり"], pos:"その他", category:"会話" },
+
+  { en:"throughout", jp:["〜の間ずっと"], pos:"その他", category:"時間" },
+
+  { en:"up to", jp:["〜まで"], pos:"その他", category:"程度" },
+
+  { en:"with regard to", jp:["〜に関して"], pos:"その他", category:"会話" },
+  { en:"with respect to", jp:["〜に関して"], pos:"その他", category:"会話" },
+
+  { en:"as long as", jp:["〜する限り"], pos:"その他", category:"条件" },
+  { en:"as soon as", jp:["〜するとすぐに"], pos:"その他", category:"時間" },
+  { en:"as far as", jp:["〜する限り"], pos:"その他", category:"条件" },
+
+  { en:"even if", jp:["たとえ〜でも"], pos:"その他", category:"接続" },
+  { en:"only if", jp:["〜の場合にのみ"], pos:"その他", category:"条件" },
+
+  { en:"unless", jp:["〜しない限り"], pos:"その他", category:"接続" },
+  { en:"while", jp:["〜する一方で"], pos:"その他", category:"接続" },
+  { en:"whereas", jp:["〜である一方"], pos:"その他", category:"接続" },
+  { en:"whether", jp:["〜かどうか"], pos:"その他", category:"接続" }
+];
